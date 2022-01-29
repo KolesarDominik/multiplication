85adced3ddbd11e9c3595ae24218043b93d20817
Dominik Kolesar <dominik.kolesar@gmail.com>
2022-01-29 07:06:32 +0100

    centering configuration rows, using unsplash and picsum images, adding random request paremeter v to prevent caching

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const byId = (ident) => document.getElementById(ident);
 const parseInteger = (value) => parseInt(value, 10);
 const randomInRange = (min, max) => Math.floor(Math.random() * (max-min)) + min;
-// const imageSize = () => console.log(issues.style.background
-
-
-const issues = byId('issues');
 
 const fixed1 = byId('fixed_number1');
 const min1 = byId('min_number1');
@@ -14,27 +10,19 @@ const fixed2 = byId('fixed_number2');
 const min2 = byId('min_number2');
 const max2 = byId('max_number2');
 
+const issues = byId('issues');
 const compute = byId('compute_button');
 
-// const thewindow = window;
-
+const getImageUnsplash = (width, height, v) => `https://source.unsplash.com/${width}x${height}?v=${v}`;
+const getImagePicsum = (width, height, v) => `https://picsum.photos/${width}/${height}?v=${v}`;
 const setRandomBackground = () => {
-    const background = window.getComputedStyle(issues).background.includes('1000');
-    issues.style.backgroundImage = background ? "url('https://source.unsplash.com/1100x1100')" : "url('https://source.unsplash.com/1000x1000')";
+    const height = parseInteger(window.screen.height/(Math.random()+2));
+    const width = parseInteger(window.screen.width/(Math.random()+2));
+    const v = Math.random();
+    const isUnsplash = window.getComputedStyle(issues).background.includes('unsplash');
+    issues.style.backgroundImage =  isUnsplash ?  `url('${getImagePicsum(width, height, v)}')` : `url('${getImageUnsplash(width, height, v)}')`;
 };
 
-
-// [fixed1, min1, max1, fixed2, min2, max2].forEach(function(input) {
-//     input.addEventListener('change', function(e) {
-//       if (e.target.value == '') {
-//         e.target.value = 0
-//       }
-//     })
-// })
-
-// min1.onchange = (v) => {
-//     console.log(v);
-// }
 
 compute.onclick = () => {
     const range1 = fixed1.value ? [fixed1.value, fixed1.value] : [min1.value || 0, max1.value];
@@ -58,5 +46,3 @@ compute.onclick = () => {
     
 
 };
-
-// setRandomBackground(issues);
